960f8bf9094c13ec29a5bef9075d37fc2165de7a
Kazuhiro Takanami <k.highwave@gmail.com>
2021-10-04 15:03:58 +0900

    heroku opt

--- server/index.js
+++ server/index.js
@@ -4,9 +4,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(compression());
-app.use(express.static(__dirname + '/../dist'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(port);
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/../dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 console.log(`Server listening on ${port}`);
